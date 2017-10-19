fe8678eb67a608c10f6413d376fbd612557eefc8
juani <juanignaciocabrera@live.com>
2017-10-19 11:33:34 -0300

    Mapeo hibernate

--- src/main/java/Controlador/PoliticasContraseniaDAO.java
+++ src/main/java/Controlador/PoliticasContraseniaDAO.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Modelo;
+package Controlador;
 
 /**
  *
  * @author juani
  */
-public class AulaInformatica extends Aula{
-    private int cantidadPCs;
-    private boolean canion;
+public class PoliticasContraseniaDAO {
+    
 }
