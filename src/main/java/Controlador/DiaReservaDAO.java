ffcc6db4c897584895f7f503e9c6553d0755eb51
Fausto <faustofedele2013@gmail.com>
2017-12-07 21:03:07 -0300

    CREO que esta lo de la dispoibilidad, recuerden que es de a una que verifica

--- src/main/java/Controlador/DiaReservaDAO.java
+++ src/main/java/Controlador/DiaReservaDAO.java
@@ -5,13 +5,35 @@
  */
 package Controlador;
 
+import Modelo.Diareserva;
+import java.util.Date;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 /**
  *
  * @author Fausto
  */
 public class DiaReservaDAO extends GenericDAO{
 
+    private HibernateUtil HU;
+    private Session SS;
+    
     public DiaReservaDAO() {
+    }
+    
+    public List<Diareserva> getReservas(int aulaID, Date fecha, Date h_in, Date h_fin){ // DEVUELVE LAS POSIBLES
+        SS = HU.getSessionFactory().openSession();
+        SS.beginTransaction();
+        String sentencia = "SELECT * FROM diareserva D WHERE D.aulaID=" + aulaID + " AND D.fecha= " + fecha + " "
+                + "AND ((D.horainicio<=" + h_in + " AND D.horafin>" + h_in + ") OR (D.horainicio<" + h_fin + " "
+                + "AND D.horafin>=" + h_fin + "));";
+        Query query = SS.createSQLQuery(sentencia).addEntity(Diareserva.class);
+        List<Diareserva> lista = query.list();
+        SS.getTransaction().commit();
+        SS.close();
+        return lista;
     }
     
 }
--- src/main/java/Controlador/GestorDeAula.java
+++ src/main/java/Controlador/GestorDeAula.java
@@ -6,7 +6,13 @@
 package Controlador;
 
 import Modelo.Aula;
-import java.awt.List;
+import Modelo.Diareserva;
+import Modelo.FechalectivasId;
+import Modelo.Periodo;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Date;
 
 /**
  *
@@ -33,7 +39,52 @@ public class GestorDeAula {
         return false;
     }
 
-    public boolean validarDisponibilidad(Aula a){
+    public boolean validarDisponibilidad(Date fecha, Date h_inicio, Date h_fin, String tipoDeAula, int cantAlumnos, Periodo periodo){
+        AulasDAO adao = new AulasDAO();
+        DiaReservaDAO drdao = new DiaReservaDAO();
+        boolean aulaDisponible=false;
+        int cont = -1;
+        List<Aula> listaAulas = adao.getPosibles(tipoDeAula, cantAlumnos);
+        if (!listaAulas.isEmpty()){
+            if(fecha==null){//Periodica
+                boolean aulaDisponibleAux=true;
+                while(!aulaDisponible || cont<listaAulas.size()){
+                    cont++;
+                    Date fechaCiclo;
+                    FechalectivasId fl = new FechalectivasId();
+                    switch (periodo){
+                        case PRIMERC:
+                            fechaCiclo = fl.getFechafin1c();
+                            break;
+                        default:
+                            fechaCiclo = fl.getFechafin2c();
+                            break;
+                    }
+                    while(aulaDisponibleAux && fecha.before(fechaCiclo)){
+                        Calendar c = Calendar.getInstance();
+                        c.setTime(fecha);
+                        c.add(Calendar.DATE, 7);
+                        fecha=c.getTime();
+                        List<Diareserva> reservas = drdao.getReservas(listaAulas.get(cont).getAulaId(), fecha, h_inicio, h_fin);
+                        if (!reservas.isEmpty()){
+                            aulaDisponibleAux=false;
+                        }
+                    }
+                }
+            }else{//Esporadica
+                while(!aulaDisponible || cont<listaAulas.size()){
+                    cont++;
+                    List<Diareserva> reservas = drdao.getReservas(listaAulas.get(cont).getAulaId(), fecha, h_inicio, h_fin);
+                    if (reservas.isEmpty()){
+                        aulaDisponible=true;
+                    }
+                }
+                return aulaDisponible;
+            }
+        }else{
+            //NO HAY AULA DE ESE TIPO Y CANTIDAD
+            return false;
+        }
         return false;
     }
 
@@ -42,7 +93,7 @@ public class GestorDeAula {
     }
 
     public List obtenerAula(String num, String tipo, String cap){ //YO CAMBIE LOS PARAMETROS, este es para BuscarAula
-        return new List();
+        return new ArrayList();
     }
 
     public Aula obtenerAula(String num, int piso, String tipo, String cap, String estado){ //Este lo cree yo, es para ModifciarOEliminarAula
